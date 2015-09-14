313a702fcaea9a19c8d703f61bc20398146f9d13
Steven Mackey <steven@Stevens-iMac.local>
2015-09-14 00:53:21 -0700

    Added basic UI

--- KernValleySunPhoneDirectory/ViewController.h
+++ KernValleySunPhoneDirectory/ViewController.h
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UITableView *resultsTableView;
 
 
 @end
--- KernValleySunPhoneDirectory/ViewController.m
+++ KernValleySunPhoneDirectory/ViewController.m
@@ -9,19 +9,65 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) NSArray* resultsArray;
+@property (strong, nonatomic) NSArray* possibleArray;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.possibleArray = [[NSArray alloc] initWithObjects:@"apple", @"Samsung", @"HTC", @"LG", @"Moto", @"lel", nil];
+    self.resultsArray = [[NSArray alloc] initWithArray:self.possibleArray];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
+                                   initWithTarget:self
+                                   action:@selector(dismissKeyboard)];
+    tap.cancelsTouchesInView = NO;
+    [self.view addGestureRecognizer:tap];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma Results Table
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.resultsArray count];
+}
+
+- (UITableViewCell*)tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath*)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil)
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    
+    cell.textLabel.text = [self.resultsArray objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+#pragma Search Bar
+- (void) filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope {
+    NSPredicate* predicate = [NSPredicate predicateWithFormat:@"SELF beginswith[c] %@", searchText];
+    self.resultsArray = [self.possibleArray filteredArrayUsingPredicate:predicate];
+}
+
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    [self filterContentForSearchText:searchText scope:nil];
+    NSLog(@"text did change!");
+    NSLog(searchText);
+    [self.resultsTableView reloadData];
+}
+
+//Called when user clicks on the results. Via gesture recognizer
+-(void)dismissKeyboard {
+    [self.searchBar resignFirstResponder];
 }
 
 @end
